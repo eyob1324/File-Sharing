747382fbab80c5f84fd2188c72edc2112346ae78
Eyob keflinkiel <eyob.keflinkiel@gmail.com>
2021-03-28 18:03:45 -0400

    initial push

---
+++ FileClient/src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void btnOnPressdownload(ActionEvent actionEvent) {
+    }
+
+    public void btnOnPressUpload(ActionEvent actionEvent) {
+    }
+}
